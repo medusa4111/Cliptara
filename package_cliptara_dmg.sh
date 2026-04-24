3ab9c775b22e2a6e1e5021a7ebdce9630548d8fc
Максим <maksim@Mac-mini-Maksim.local>
2026-04-24 13:21:05 +0300

    Release 1.4.1

--- package_cliptara_dmg.sh
+++ package_cliptara_dmg.sh
@@ -120,9 +120,9 @@ cat >"$APP_DIR/Contents/Info.plist" <<PLIST
   <key>CFBundlePackageType</key>
   <string>APPL</string>
   <key>CFBundleShortVersionString</key>
-  <string>1.4.0</string>
+  <string>1.4.1</string>
   <key>CFBundleVersion</key>
-  <string>10</string>
+  <string>11</string>
   <key>CliptaraUpdateManifestURL</key>
   <string>$UPDATE_MANIFEST_URL</string>
   <key>LSMinimumSystemVersion</key>
